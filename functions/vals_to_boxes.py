bb7d85306eda81c938fa1b573c09c3038c47e4b7
Maximilian Barth <maximilian1barth@gmail.com>
2021-11-26 13:11:03 +0100

    added correlation plot notebook with fixes

--- functions/vals_to_boxes.py
+++ functions/vals_to_boxes.py
@@ -6,8 +6,9 @@ boxes contain sum of values contained within
 !!! when plotting: don't forget to use origin="lower"
 -> plt.imshow(a, origin="lower")
 '''
-def vals_to_boxes(x, y, vals, x_min, x_max, y_min, y_max, n):
+def vals_to_boxes(x, y, vals, x_min, x_max, y_min, y_max, n, avg=False, remove_empty=True):
     a = np.zeros((n, n))
+    count = np.zeros((n, n))
 
     x_range = np.linspace(x_min, x_max, n + 1)
     y_range = np.linspace(y_min, y_max, n + 1)
@@ -20,6 +21,20 @@ def vals_to_boxes(x, y, vals, x_min, x_max, y_min, y_max, n):
                 val = vals[k]
 
                 if lo >= x_range[j] and lo < x_range[j + 1] and la >= y_range[i] and la < y_range[i + 1]:
-                    a[i][j] += val
+                    a[i][j] += float(val)
+                    count[i][j] += 1.0
     
+    if avg:
+        for i in range(n):
+            for j in range(n):
+                if count[i][j] > 0:
+                    a[i][j] /= count[i][j]
+
+    # remove empty cells
+    if remove_empty:
+        for i in range(n):
+            for j in range(n):
+                if count[i][j] == 0:
+                    a[i][j] = np.nan
+
     return a
